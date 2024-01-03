f926ac242ded2de9d334086f0e775e73b6b668f2
devwael <dev.ahmedwael@gmail.com>
2024-01-03 04:36:24 +0200

    Add Admin options

--- src/Main.php
+++ src/Main.php
@@ -7,6 +7,7 @@ if ( ! defined( '\ABSPATH' ) ) {
 	exit;
 }
 
+use DevWael\WpPostsReceiver\Admin\AdminOptions;
 use DevWael\WpPostsReceiver\Languages\Localization;
 use DevWael\WpPostsReceiver\Rest\EndPoint;
 
@@ -28,6 +29,11 @@ class Main {
 	private EndPoint $endpoint;
 
 	/**
+	 * @var AdminOptions
+	 */
+	private AdminOptions $admin_options;
+
+	/**
 	 * Get instance
 	 *
 	 * @return self|null
@@ -44,8 +50,9 @@ class Main {
 	 * Constructor
 	 */
 	private function __construct() {
-		$this->localization = new Localization();
-		$this->endpoint     = new Endpoint();
+		$this->localization  = new Localization();
+		$this->endpoint      = new Endpoint();
+		$this->admin_options = new AdminOptions();
 	}
 
 	/**
@@ -56,5 +63,6 @@ class Main {
 	public function init(): void {
 		$this->localization->load_hooks();
 		$this->endpoint->load_hooks();
+		$this->admin_options->load_hooks();
 	}
 }
