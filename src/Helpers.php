b9fbd4f90a329b039d3a99ab890544bfb1ac0f83
devwael <dev.ahmedwael@gmail.com>
2024-01-10 04:39:12 +0200

    Remove unneeded functions

--- src/Helpers.php
+++ src/Helpers.php
@@ -24,18 +24,6 @@ class Helpers {
 	}
 
 	/**
-	 * Get all ACF post fields
-	 *
-	 * @param int  $post_id      Post ID.
-	 * @param bool $format_value Format value.
-	 *
-	 * @return mixed
-	 */
-	public static function get_acf_post_fields( $post_id = false, $format_value = true ) {
-		return function_exists( '\get_fields' ) ? \get_fields( $post_id, $format_value ) : false;
-	}
-
-	/**
 	 * Update ACF field
 	 *
 	 * @param string $selector ACF field selector.
@@ -46,36 +34,6 @@ class Helpers {
 	 */
 	public static function update_acf_field( $selector, $value, $post_id = false ) {
 		return function_exists( '\update_field' ) ? \update_field( $selector, $value, $post_id ) : false;
-	}
-
-	/**
-	 * Get sites rest endpoint.
-	 *
-	 * @return string Sites rest endpoint.
-	 */
-	public static function remote_site_rest_endpoint(): string {
-		/**
-		 * Filter remote site endpoint.
-		 *
-		 * @since 1.0.0
-		 */
-		return apply_filters( 'wp_posts_sender_remote_site_endpoint', 'wp-posts-sender/v1/add-post' );
-	}
-
-	/**
-	 * Get supported post types.
-	 *
-	 * @return array
-	 */
-	public static function supported_post_types(): array {
-		/**
-		 * Filter supported post-types.
-		 *
-		 * @param array $post_types Supported post-types.
-		 *
-		 * @ssince 1.0.0
-		 */
-		return \apply_filters( 'wp_posts_sender_supported_post_types', [ 'post', 'product' ] );
 	}
 
 	/**
